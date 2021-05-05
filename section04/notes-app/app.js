967d9ad73ad2244e3f7186212e1598c978f476f9
DongKyu <po4tion0429@gmail.com>
2021-05-05 20:34:46 +0900

    section04 yargs

---
+++ section04/notes-app/app.js
@@ -0,0 +1,49 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./note.js');
+
+// yargs 버전 세팅
+yargs.version('1.1.0');
+
+// add 명령어
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: () => console.log('Adding a new note!'),
+});
+
+// remove 명령어
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => console.log('Removing the note'),
+});
+
+// list 명령어
+yargs.command({
+  command: 'list',
+  describe: 'list your notes',
+  handler: () => console.log('Listing out all note'),
+});
+
+// read 명령어
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => console.log('Reading a note'),
+});
+
+console.log(yargs.argv);
+
+/* Section04 - 15
+const chalk = require('chalk');
+const getNotes = require('./note.js');
+
+const command = process.argv[2];
+
+if (command === 'add') {
+  console.log('Adding note!');
+} else if (command === 'remove') {
+  console.log('Removing note!');
+}
+*/
