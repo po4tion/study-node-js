d470597fc0709bd750a1ef994dbd6d8c872e1e47
DongKyu <po4tion0429@gmail.com>
2021-05-05 21:28:06 +0900

    section04 - 18

--- section04/notes-app/app.js
+++ section04/notes-app/app.js
@@ -9,7 +9,22 @@ yargs.version('1.1.0');
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: () => console.log('Adding a new note!'),
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: (argv) => {
+    console.log('Title: ', argv.title);
+    console.log('Body: ', argv.body);
+  },
 });
 
 // remove 명령어
