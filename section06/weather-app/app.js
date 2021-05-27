7abd686ab1e4cdbeb3d082fdcff45e70c1a8a5c1
DongKyu <po4tion0429@gmail.com>
2021-05-27 20:13:39 +0900

    print data

--- section06/weather-app/app.js
+++ section06/weather-app/app.js
@@ -3,8 +3,9 @@ const request = require('request');
 const url =
   'http://api.weatherstack.com/current?access_key=9af5b906c999cccad566ff5cf5720a10&query=Seoul';
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
+request({ url: url, json: true }, (error, response) => {
+  const data = response.body.current;
+  const print = `It is currently ${data.temperature} degrees out. There is a ${data.precip}% chance of rain.`;
 
-  console.log(data.current);
+  console.log(print);
 });
